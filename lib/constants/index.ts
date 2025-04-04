cded04ef10c4471ed665cb95309b340235c3921c
Artem Strohin <artem.strohin@gmail.com>
2025-04-04 19:08:13 +0100

    initial commit

---
+++ lib/constants/index.ts
@@ -0,0 +1,3 @@
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || "Store";
+export const APP_DESRIPTION = process.env.NEXT_PUBLIC_APP_NAME || "A modern e-commerce store";
+export const SERVER_URL = process.env.NEXT_PUBLIC_SERVER_URL || "http://localhost:3000";
