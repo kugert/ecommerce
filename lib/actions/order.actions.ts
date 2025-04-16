20a224a1de3a5a2c3c45a032bfcf589e2d267168
Artem Strohin <artem.strohin@gmail.com>
2025-04-16 13:51:55 +0100

    feat: add user profile management with update functionality and pagination for order history

--- lib/actions/order.actions.ts
+++ lib/actions/order.actions.ts
@@ -10,6 +10,7 @@ import { prisma } from "@/db/prisma";
 import { CartItem, PaymentResult } from "@/types";
 import { paypal } from "@/lib/paypal";
 import { revalidatePath } from "next/cache";
+import { PAGE_SIZE } from "@/lib/constants";
 
 export async function createOrder(){
     try {
@@ -237,6 +238,32 @@ async function updateOrderToPaid({
         },
     });
     if (!updatedOrder) throw new Error("Order not found");
+}
 
+// Get user orders
+export async function getMyOrders({
+  limit = PAGE_SIZE,
+  page = 1,
+}: {
+  limit?: number;
+  page: number;
+}) {
+   const session = await auth();
+   if (!session) throw new Error("User is not authenticated");
 
+   const data = await prisma.order.findMany({
+     where: { userId: session?.user?.id },
+     take: limit,
+     skip: (page - 1) * limit,
+     orderBy: { createdAt: "desc" },
+   });
+
+   const dataCount = await prisma.order.count({
+     where: { userId: session?.user?.id },
+   });
+
+   return {
+     data,
+     totalPages: Math.ceil(dataCount / limit),
+   }
 }
